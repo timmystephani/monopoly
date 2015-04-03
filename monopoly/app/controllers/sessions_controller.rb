1c746ed5b236c2ed832689f158d4a9d456015b34
Tim Stephani [Ubuntu Desktop] <timmy.stephani@gmail.com>
2015-04-03 00:16:46 -0400

    added error and success display

--- monopoly/app/controllers/sessions_controller.rb
+++ monopoly/app/controllers/sessions_controller.rb
@@ -5,9 +5,11 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by_email(params[:user][:email])
     if @user && @user.authenticate(params[:user][:password])
+      flash[:notice] = 'Successfully created an account'
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
+      flash[:error] = 'There was a problem creating your account'
       redirect_to '/'
     end
   end
